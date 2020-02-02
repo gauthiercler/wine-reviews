44e2810d98690042eb2dc96d10395f919c3b3180
Gauthier Cler <gauthier.cler@epitech.eu>
2020-02-02 01:37:27 +0100

    update recommender

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import pandas as pd
 import streamlit as st
-from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
 
 
@@ -13,6 +13,8 @@ def load_data():
 
 
 def main():
+
+    st.title('Wine recommender')
     data = load_data().copy(deep=True)
     data.dropna(subset=['country', 'variety'], axis=0, inplace=True)
 
@@ -20,26 +22,28 @@ def main():
     price = st.slider('Price range', 0.0, max_price, 20.0)
     country = st.selectbox('Country', ['All'] + data['country'].unique().tolist())
     variety = st.selectbox('Variety', ['All'] + data['variety'].unique().tolist())
-    keywords = st.text_area('Keywords', '')
+    keywords = st.text_area('Keywords', 'spicy strong berry')
 
     if st.button('Search'):
-        vectorizer = CountVectorizer()
-        matrix = vectorizer.fit_transform(data['description'].append(pd.Series(keywords)))
-        similarities = cosine_similarity(matrix[-1], matrix).flatten()
-        data['similarity'] = similarities[:-1]
+        with st.spinner('Hold on, looking for wines matching your dearest wishes...'):
+            vectorizer = TfidfVectorizer()
+            matrix = vectorizer.fit_transform(data['description'].append(pd.Series(keywords)))
+            similarities = cosine_similarity(matrix[-1], matrix).flatten()
+            data['similarity'] = similarities[:-1]
 
-        data = data[data['price'].between(price, max_price)]
-        if country != 'All':
-            data = data[data['country'] == country]
+            data = data[data['price'].between(price, max_price)]
+            if country != 'All':
+                data = data[data['country'] == country]
 
-        if variety != 'All':
-            data = data[data['variety'] == variety]
+            if variety != 'All':
+                data = data[data['variety'] == variety]
 
-        data.sort_values(by='similarity', ascending=False, inplace=True)
+            data = data[data.similarity > 0]
+            data.sort_values(by='similarity', ascending=False, inplace=True)
 
-        results = data[['country', 'description', 'points', 'price', 'title', 'variety', 'similarity']].head(
-            10).reset_index(drop=True)
-        st.table(results)
+            results = data[['country', 'description', 'points', 'price', 'title', 'variety', 'similarity']].head(
+                10).reset_index(drop=True)
+            st.table(results)
 
 
 if __name__ == '__main__':
